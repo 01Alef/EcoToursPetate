ac16ca2204bb7be98d8b94e54fa8ff56c3ae6351
Allan Pineda <67522162+01Alef@users.noreply.github.com>
2023-09-24 19:55:09 -0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+function enviarForm1() {
+    let name_usuario = document.getElementById('nombre1').value;
+    let mail_usuario = document.getElementById('mail1').value;
+
+
+    alert("Muchas Gracias " + name_usuario + " por ponerse en contacto con nosotros, enviaremos una pronta respuesta al correo " + mail_usuario);
+    
+}
